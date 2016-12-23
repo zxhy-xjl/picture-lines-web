705511dfed2739ebcd7249436eb84a564f089826
guanzheyuan <834155417@qq.com>
2016-12-23 13:44:46 +0800

    update

--- PiclineManagement/src/main/java/com/zxhy/xjl/pic/ServletPic.java
+++ PiclineManagement/src/main/java/com/zxhy/xjl/pic/ServletPic.java
@@ -50,52 +50,42 @@ public class ServletPic extends HttpServlet {
 		HttpSession session = request.getSession();
 		String flag = request.getParameter("hid");
 		//0是生成图片二维码，1是验证
-		if(flag.equals("0"))
-		{	
-		System.out.println( request.getParameter("file1"));
-		String logoAddress =request.getParameter("file2");
-		String picAddress =request.getParameter("file1");
-		int n = Integer.parseInt(request.getParameter("sec1")) ;
-		QRPicServiceImpl qrps=new QRPicServiceImpl();
-		try {
-			sucflag="二维码加密成功！";
-			qrps.getQRPic("ZTEsoft", logoAddress, "d:/pic/newlogo.jpg", false, picAddress, 10, 10, 1.0f,n);
-			session.setAttribute("sucflag", sucflag);
-			response.sendRedirect("examples/picmanager.jsp");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			sucflag="加密失败，请检查输入文件！";
-			e.printStackTrace();
-			session.setAttribute("sucflag", sucflag);
-			response.sendRedirect("examples/picmanager.jsp");
-
-		}
-		}
-		else if(flag.equals("1"))
-		{
+		if(flag.equals("0")){	
+			System.out.println( request.getParameter("file1"));
+			String logoAddress =request.getParameter("file2");
+			String picAddress =request.getParameter("file1");
+			int n = Integer.parseInt(request.getParameter("sec1")) ;
+			QRPicServiceImpl qrps=new QRPicServiceImpl();
+			try {
+				sucflag="二维码加密成功！";
+				qrps.getQRPic("ZTEsoft", logoAddress, "/Users/Arthur/Desktop/newlogo.jpg", false, picAddress, 10, 10, 1.0f,n);
+				session.setAttribute("sucflag", sucflag);
+				response.sendRedirect("examples/picmanager.jsp");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				sucflag="加密失败，请检查输入文件！";
+				e.printStackTrace();
+				session.setAttribute("sucflag", sucflag);
+				response.sendRedirect("examples/picmanager.jsp");
+			}
+		}else if(flag.equals("1")){
 			String srcFile =request.getParameter("file3");
 			String destFile =request.getParameter("file4");
 			int n = Integer.parseInt(request.getParameter("sec2")) ;
 			QRPicServiceImpl qrps=new QRPicServiceImpl();
 			boolean enflag = qrps.offlineCheck(srcFile, destFile, n);
-			if(enflag)
-			{   trflag="是真实图片，没有篡改!";
+			if(enflag){   trflag="是真实图片，没有篡改!";
 				session.setAttribute("sucflag", trflag);
 				response.sendRedirect("examples/picmanager.jsp#tabs1-js");
 				
-			}
-			else
-			{
+			}else{
 				trflag="图片被篡改，请注意信息安全！";
 				session.setAttribute("sucflag", trflag);
 				response.sendRedirect("examples/picmanager.jsp#tabs1-js");
 			}
-		} 
-		else 
-		{   trflag="输入了非法信息，请检查！";
+		}else{   trflag="输入了非法信息，请检查！";
 			session.setAttribute("sucflag", trflag);
 			response.sendRedirect("examples/picmanager.jsp");
 		}
 	}
-
 }
